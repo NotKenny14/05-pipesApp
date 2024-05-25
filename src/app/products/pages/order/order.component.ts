ea58afe7ae67567e594616cbefd5941232850f30
carlos <carlos@gmail.com>
2024-05-24 19:10:16 -0600

    first commit

---
+++ src/app/products/pages/order/order.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-order',
+  templateUrl: './order.component.html',
+  styles: [
+  ]
+})
+export class OrderComponent {
+
+}
